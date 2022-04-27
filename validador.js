66c0732ee9d91841afcddf6297a06f8b488fe015
Luis Pastén <luispasten.dev@gmail.com>
2022-04-26 19:58:32 -0500

    Fix lib

--- validador.js
+++ validador.js
@@ -1,94 +1,77 @@
-class Validation {
-
+class Validator {
+    libRules = '';
     rules = {};
     errors = {};
 
+    constructor() {
+        this.libRules = new Rules();
+    }
+
     setRules(rules) {
-        this.rules = rules
-        return this;
+        this.rules = rules;
+        // return this;
     }
 
     run(data) {
+        this.errors = {};
         for (const field in data) {
             if (field in this.rules) {
-                this.callRules(this.rules[field], field, data[field]);
+                this.callRules(this.rules[field], data[field], field);
             }
         }
         return !(Object.keys(this.errors).length > 0);
     }
 
-    callRules(rules, field, data) {
-        
-        const matchs     = rules.split('|');
-        const groupRules = new Rules();
-
+    callRules(rules, data, field) {
+        const matchs = rules.split('|');
         matchs.forEach(match => {
-            if (typeof groupRules[match] !== 'function') {
-                throw `${match} no es una regla.`;
+            if (typeof this.libRules[match] !== 'function') {
+                throw `${match} no es una regla`;
             }
-            if (!groupRules[match](data)) { // run rules
+            if (!this.libRules[match](data)) {
                 this.errors[field] = {
                     ...this.errors[field],
                     [match]: this.messages(match)
                 }
             }
         });
-        
-        return this.errors;
     }
 
     messages(key) {
         const messages = {
-            required: 'Requerido mi joven',
-            is_number: 'No es un numero mi joven',
-            bool: ''
+            required: 'Campo requerido',
+            is_valid_age: 'El campo debe tener una edad valida'
         }
         return messages[key];
     }
 
-    reset() {
-        this.errors = {};
-    }
-
-    get getErrors() {
+    getErrors() {
         return this.errors;
     }
+
 }
 
 class Rules {
-
     required(data) {
-        return !(data === '');
+        data = data.toString();
+        return data.trim() !== '';
     }
-
-    is_number(data) {
-        return !isNaN(data);
+    is_valid_age(data) {
+        return /^[1-9]?[0-9]{1}$|^100$/.test(data);
     }
-
-    bool(data) {
-        return false;
-    }
-
 }
 
-// ===================== CLIENT CODE ============================
-const validation = new Validation();
-if (!validation
-    .setRules({
-        amount: 'is_number|required|bool', 
-        description: 'required'
-    })
-    .run({
-        amount: "el tre me", 
-        description: ""
-    })) {
-        console.log(validation.getErrors)
-        // {   
-        //     amount: {
-        //       is_number: 'No es un numero mi joven' 
-        //     },   
-        //     description: { 
-        //       required: 'Requerido mi joven' 
-        //     }   
-        // }
-    };
+// client code =============================
+
+const validator = new Validator();
+validator.setRules({
+    name: 'required',
+    age: 'required|is_valid_age'
+});
+
+if (!validator.run({
+    name: '',
+    age: ''
+})) {
+    console.log(validator.getErrors());
+}
